87e7d411770b27a8986403db336a7fbeb5aead68
danieljaeim <imdanieljae@gmail.com>
2020-12-01 23:31:00 -0800

    pushing working secret-hitler pinging

---
+++ commands/init.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'init',
+	description: 'initializes the secret hitler instance',
+	execute(message, args) {
+        message.channel.send('Initializing a new game. React to this post if you are interested in playing.');
+        // Here we will initialize a new game of secret hitler. 
+
+        /** 
+         * 1. 
+         */
+	},
+};
--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-// require the discord.js module
+const fs = require('fs');
 const Discord = require('discord.js');
-const { token, prefix } = require('./config.json');
+const { prefix, token } = require('./config.json');
 
-// create a new Discord client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Secret Hitler bot is ready!');
 });
 
 client.on('message', message => {
-    if (message.content == 'ping') {
-        message.channel.send('pong');
-    }
-})
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-// login to Discord with your app's token
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	// other commands...
+});
+
 client.login(token);
