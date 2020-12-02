cd28a86f1f956d02b881e22c35332f05d68a573a
danieljaeim <imdanieljae@gmail.com>
2020-12-02 00:16:35 -0800

    create text-channel using start command

--- commands/start.js
+++ commands/start.js
@@ -1,12 +1,20 @@
 module.exports = {
-	name: 'start',
-	description: 'initializes the secret hitler instance',
-	execute(message, args) {
-        message.channel.send('Initializing a new game. React to this post if you are interested in playing.');
-        // Here we will initialize a new game of secret hitler. 
+  name: "start",
+  description: "initializes the secret hitler instance",
+  execute(client, message, args) {
+        const hostName = message.author.username;
+        message.guild.channels.create(`${hostName}'s-secret-hitler-game`, {
+                type: 'text'
+        })
+        .then(channel => {
+                console.log(channel.name)
+        })
+        message.channel.send(`${hostName} has started a Secret Hitler game. Join the new text-channel made by ${hostName}!`
+    );
+    // Here we will initialize a new game of secret hitler.
 
-        /** 
-         * 1. 
-         */
-	},
+    /**
+     * 1.
+     */
+  },
 };
