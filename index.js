4921af31d8e634cc5baf8c80d2ca8fc506ad5721
danieljaeim <imdanieljae@gmail.com>
2020-12-01 23:12:21 -0800

    pushing default index.js file

---
+++ index.js
@@ -0,0 +1,14 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// login to Discord with your app's token
+client.login('your-token-goes-here');
