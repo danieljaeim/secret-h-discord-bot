9164e4845b531e5d35af7592589802fcd6549227
danieljaeim <imdanieljae@gmail.com>
2020-12-03 22:25:17 -0800

    pushing to github

--- commands/start.js
+++ commands/start.js
@@ -14,29 +14,28 @@ module.exports = {
     let hostChannel; // this is the channel where the game is hosted
 
     /** Create the text-channel that will host the game. If the text channel already exists, do not create it. */
-    if (
-      !message.guild.channels.cache.some((c) => {
-        c.name.trim();
-        return c.name == hostChannelName;
-      })
-    ) {
-      message.guild.channels
-        .create(hostChannelName, {
-          type: "text",
-        })
-        .then((channel) => {
-          console.log("created a new room called " + channel.name);
-          hostChannel = channel;
-        });
+    if (!message.guild.channels.cache.some((c) => c.name == hostChannelName)) {
+      message.guild.channels.create(hostChannelName, {
+        type: "text",
+	  })
+	  .then(channel => {
+		console.log("created a new room called " + channel);
+		hostChannel = channel;
+		message.channel.send(createEmbed(hostName, hostChannel));
+	  })
     } else {
-		hostChannel = message.guild.channels.cache.find(c => c.name == hostChannelName)
-    }
-
-    message.channel.send(createEmbed(hostName, hostChannel));
-    // Here we will initialize a new game of secret hitler.
+      hostChannel = message.guild.channels.cache.find(
+        (c) => c.name == hostChannelName
+	  );
+	  message.channel.send(createEmbed(hostName, hostChannel));
+	}
+	// Here we will initialize a new game of secret hitler.
 
     /**
-     * 1.
+     * 1. Send an embed in the channel that we just made.
+     * 2. People can enter the text channel and type @sh.join to enter the game's queue
+     * 3. Once enough people are ready, the host types @sh.ready
+     * 4. Bot PM'S them their roles, and they begin the game.
      */
   },
 };
--- embeds/startEmbed.js
+++ embeds/startEmbed.js
@@ -6,5 +6,7 @@ module.exports = createStartEmbed = (username, channel) => new Discord.MessageEm
 	.setColor('#0099ff')
 	.setTitle(`Secret Hitler Starting!`)
 	.setAuthor(username, 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription(`A secret hitler game is starting in the <#${channel.id}> text channel soon.`)
-	.setThumbnail('https://i.imgur.com/1jE7uUZ.jpg')
+	.setDescription(`A secret hitler game is starting in the ${channel} text channel. Join the lobby and the host will begin!`)
+    .setThumbnail('https://i.imgur.com/1jE7uUZ.jpg')
+    
+    // maybe an indicator of the number of people currently on the game
